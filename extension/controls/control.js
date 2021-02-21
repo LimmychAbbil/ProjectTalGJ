91410f70a8ad6426dba2e10f503082d560b72311
Lim <limych24@gmail.com>
2021-02-21 19:13:54 +0200

    0.1.1: Implement the extension main logic

---
+++ extension/controls/control.js
@@ -0,0 +1,45 @@
+
+addJiraLink();
+
+function addJiraLink() {
+	var divLinesWithBranches = document.getElementsByClassName("flex-auto min-width-0 mb-2");
+	var targetDiv = null;
+	if (divLinesWithBranches.length >= 1) {
+		targetDiv = divLinesWithBranches[0];
+		var divWithBranchName = targetDiv.getElementsByClassName("commit-ref css-truncate user-select-contain expandable head-ref")[0];
+		
+		var branchName = divWithBranchName.textContent;
+		
+		var jiraIdFound = getJiraID(branchName)[0];
+		
+		if (jiraIdFound == null) return;
+		
+		var jiraId = jiraIdFound.replaceAll("/", "").replaceAll("_", "");
+		
+		var nodeButton = document.createElement("img");
+		
+		targetDiv.appendChild(createImageButton(jiraId));
+		//console.log(createImageButton(jiraId));
+	} else {
+		return;
+	}
+}
+
+
+
+function getJiraID(branchName) {
+	const regex = /\/[A-Z,a-z]{3,}-\d{2,}(_|\/|$)/g;
+	return branchName.match(regex);
+	
+}
+
+function createImageButton(branchName) {
+	var nodeButton = document.createElement("a");
+	nodeButton.href = "https://jira.talendforge.org/browse/" + branchName;
+	var imgNode = document.createElement("img");
+	imgNode.src = chrome.runtime.getURL('icons/TalJ_16.png');
+	
+	nodeButton.appendChild(imgNode);
+	
+	return nodeButton;
+}
