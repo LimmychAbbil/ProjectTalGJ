d05d5e5845025494f204610ab8422203730e6aa0
Limmy <limych24@gmail.com>
2024-10-28 10:55:11 +0200

    0.2.0: Distinguish 2 jiras

--- extension/controls/handleCommit.js
+++ extension/controls/handleCommit.js
@@ -2,40 +2,40 @@
 addJiraLink();
 
 function addJiraLink() {
-	var divsWithCommitMessage = document.getElementsByClassName("commit-title markdown-title");
-	if (divsWithCommitMessage.length >= 1) {
-		var commitMessage = divsWithCommitMessage[0].textContent;
-		
-		var jiraIdFound = getJiraID(commitMessage);
-		
-		if (jiraIdFound == null || jiraIdFound[0] == null) return;
-		
-		var jiraId = jiraIdFound[0].replaceAll("(", "").replaceAll(")", "");
-		
-		var nodeButton = document.createElement("img");
-		
-		divsWithCommitMessage[0].insertBefore(createImageButton(jiraId), divsWithCommitMessage[0].firstChild);
-	} else {
-		return;
-	}
+    var divsWithCommitMessage = document.getElementsByClassName("commit-title markdown-title");
+    if (divsWithCommitMessage.length >= 1) {
+        var commitMessage = divsWithCommitMessage[0].textContent;
+        
+        var jiraIdFound = getJiraID(commitMessage);
+        
+        if (jiraIdFound == null || jiraIdFound[0] == null) return;
+        
+        var jiraId = jiraIdFound[0].replaceAll("(", "").replaceAll(")", "");
+        
+        var nodeButton = document.createElement("img");
+        
+        divsWithCommitMessage[0].insertBefore(createImageButton(jiraId), divsWithCommitMessage[0].firstChild);
+    } else {
+        return;
+    }
 }
 
 
 
 function getJiraID(commitMessage) {
-	const regex = /\([A-Z,a-z]{3,}-\d{2,}\)/g;
-	return commitMessage.match(regex);
-	
+    const regex = /\([A-Z,a-z]{3,}-\d{2,}\)/g;
+    return commitMessage.match(regex);
+    
 }
 
 function createImageButton(ticketName) {
-	var nodeButton = document.createElement("a");
-	nodeButton.href = "https://jira.talendforge.org/browse/" + ticketName;
-	nodeButton.style = "vertical-align: middle";
-	var imgNode = document.createElement("img");
-	imgNode.src = chrome.runtime.getURL('icons/TalJ_16.png');
-	
-	nodeButton.appendChild(imgNode);
-	
-	return nodeButton;
+    var nodeButton = document.createElement("a");
+    nodeButton.href = "https://jira.talendforge.org/browse/" + ticketName;
+    nodeButton.style = "vertical-align: middle";
+    var imgNode = document.createElement("img");
+    imgNode.src = chrome.runtime.getURL('icons/TalJ_16.png');
+    
+    nodeButton.appendChild(imgNode);
+    
+    return nodeButton;
 }
--- extension/controls/handlePR.js
+++ extension/controls/handlePR.js
@@ -1,45 +1,63 @@
+const talendJiraURL = "https://jira.talendforge.org/browse/";
+const qlikJiraURL = "https://qlik-dev.atlassian.net/browse/";
+const migrationDate = new Date("2024-10-25");
 
-addJiraLink();
-
+function getCreatedDate() {
+    var prHeaderElement = document.getElementsByClassName("timeline-comment-header clearfix d-flex");
+    if (prHeaderElement.length >= 1) {
+        var createdDateTimestamp = prHeaderElement[0].getElementsByTagName("relative-time")[0].getAttribute("datetime");
+        return new Date(createdDateTimestamp);
+    } else {
+        return;
+    }
+}
 function addJiraLink() {
-	var divLinesWithBranches = document.getElementsByClassName("flex-auto min-width-0 mb-2");
-	var targetDiv = null;
-	if (divLinesWithBranches.length >= 1) {
-		targetDiv = divLinesWithBranches[0];
-		var divWithBranchName = targetDiv.getElementsByClassName("commit-ref css-truncate user-select-contain expandable head-ref")[0];
-		
-		var branchName = divWithBranchName.textContent;
-		
-		var jiraIdFound = getJiraID(branchName)[0];
-		
-		if (jiraIdFound == null) return;
-		
-		var jiraId = jiraIdFound.replaceAll("/", "").replaceAll("_", "").replace(/-$/, "");
-		
-		var nodeButton = document.createElement("img");
-		
-		targetDiv.appendChild(createImageButton(jiraId));
-	} else {
-		return;
-	}
+    var divLinesWithBranches = document.getElementsByClassName("flex-auto min-width-0 mb-2");
+    var targetDiv = null;
+    if (divLinesWithBranches.length >= 1) {
+        targetDiv = divLinesWithBranches[0];
+        var divWithBranchName = targetDiv.getElementsByClassName("commit-ref css-truncate user-select-contain expandable head-ref")[0];
+        
+        var branchName = divWithBranchName.textContent;
+        
+        var jiraIdFound = getJiraID(branchName)[0];
+        
+        if (jiraIdFound == null) return;
+        
+        var jiraId = jiraIdFound.replaceAll("/", "").replaceAll("_", "").replace(/-$/, "");
+        
+        var nodeButton = document.createElement("img");
+
+        var jiraURL = 
+        targetDiv.appendChild(createImageButton(jiraId));
+    } else {
+        return;
+    }
 }
 
 
 
 function getJiraID(branchName) {
-	const regex = /(\/|-|_)[A-Z,a-z]{3,}-\d{2,}(_|-|\/|$)/g;
-	return branchName.match(regex);
-	
+    const regex = /(\/|-|_)[A-Z,a-z]{3,}-\d{2,}(_|-|\/|$)/g;
+    return branchName.match(regex);
+    
 }
 
 function createImageButton(ticketName) {
-	var nodeButton = document.createElement("a");
-	nodeButton.href = "https://jira.talendforge.org/browse/" + ticketName;
-	nodeButton.style = "vertical-align: middle";
-	var imgNode = document.createElement("img");
-	imgNode.src = chrome.runtime.getURL('icons/TalJ_16.png');
-	
-	nodeButton.appendChild(imgNode);
-	
-	return nodeButton;
+    var nodeButton = document.createElement("a");
+    nodeButton.style = "vertical-align: middle";
+    var imgNode = document.createElement("img");
+    if (getCreatedDate() < migrationDate || (ticketName.startsWith("TDI") || ticketName.startsWith("TUP"))) {
+        imgNode.src = chrome.runtime.getURL('icons/TalJ_16.png');
+        nodeButton.href = talendJiraURL + ticketName;
+    } else {
+        imgNode.src = chrome.runtime.getURL('icons/QlkJ_16.png');
+        nodeButton.href = qlikJiraURL + ticketName;
+    }
+    
+    nodeButton.appendChild(imgNode);
+    
+    return nodeButton;
 }
+
+addJiraLink();
